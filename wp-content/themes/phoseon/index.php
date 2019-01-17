9614d2fd5471fa67eefa4e4691234ad18c018a9c
Shelly Graham <shellymariegraham@gmail.com>
2019-01-16 22:26:33 -0800

    Adjustments to blog masonry layout

--- wp-content/themes/phoseon/index.php
+++ wp-content/themes/phoseon/index.php
@@ -23,7 +23,7 @@ get_header(); ?>
 
 		</div>
 		<div class="cell small-8 small-offset-1">
-			<?php echo do_shortcode('[ajax_load_more id="blog_all" target="blog_filter" filters="true" filters_debug="true" repeater="template_1" post_type="post, press_releases, in_the_news" taxonomy="posts_year, posts_category" taxonomy_operator="IN" button_label="Load More"] '); ?>
+			<?php echo do_shortcode('[ajax_load_more id="blog_all" target="blog_filter" filters="true" filters_debug="true" repeater="template_1" post_type="post, press_releases, in_the_news" taxonomy="posts_year, posts_category" taxonomy_operator="IN" button_label="Load More" transition="masonry" masonry_selector=".grid-item" masonry_animation="slide-up" masonry_horizontalorder="false"]'); ?>
 		</div>
 	</div>
 </div>
